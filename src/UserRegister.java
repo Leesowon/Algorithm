f35522bb630627497cd79f3fa08db952308a9c3f
sowon <leesowon00@naver.com>
2023-08-09 17:54:41 +0900

    user register 2

--- src/UserRegister.java
+++ src/UserRegister.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class UserRegister {
@@ -12,7 +14,7 @@ public class UserRegister {
 
         while(!register){
             System.out.println("회원 가입을 진행하시겠습니까? \ny : 진행    n : 종료");
-            System.out.print(">>");
+            System.out.print(">> ");
             String register_input = sc.nextLine();
 
             if(register_input.equalsIgnoreCase("y")){
@@ -30,6 +32,58 @@ public class UserRegister {
             else{
                 System.out.println("<<올바른 값을 입력해주세요>>");
             }
+        }
+
+        ArrayList users = new ArrayList<>(); // 가입된 회원 저장
+
+        while (true){
+            HashMap user = new HashMap(); // 한명의 유저에 대한 정보를 담는 hashmap
+
+            // ID
+            System.out.print("ID : ");
+            String username = sc.nextLine();
+
+            // pw
+            String pw = "";
+            while (true) {
+                System.out.print("pw : ");
+                pw = sc.nextLine();
+
+                System.out.print("pw 확인 : ");
+                String pw_confirm = sc.nextLine();
+
+                if(pw.equals(pw_confirm)){
+                    break;
+                } else {
+                    System.out.println("======================");
+                    System.out.println("비밀번호가 일치하지 않습니다.");
+                    System.out.println("비밀번호를 다시 입력해주세요");
+                    System.out.println("======================");
+                }
+            }
+
+            // 이름
+            System.out.print("이름 : ");
+            String name = sc.nextLine();
+
+            // 생년월일(6자리)
+            String birth = ""; // 바깥에서 참조해야 하기 때문에 외부에서 선언 후 while 실행
+            while (true){
+                System.out.print("생년월일(6자리) : ");
+                birth = sc.nextLine();
+                if(birth.length() == 6){
+                    break;
+                } else {
+                    System.out.println("======================");
+                    System.out.println("생년월일 자릿수가 올바르지 않습니다..");
+                    System.out.println("생년월일을 다시 입력해주세요");
+                    System.out.println("======================");
+                }
+            }
+
+            // e-mail
+            System.out.print("email : ");
+            String email = sc.nextLine();
         }
     }
 }
