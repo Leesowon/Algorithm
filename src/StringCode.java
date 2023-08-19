0b6dcd65d79da6944f4f744763f65b9c1125eaf7
sowon <leesowon00@naver.com>
2023-08-19 22:07:40 +0900

    Day5-1 1, mode, ret ì--hard

--- src/StringCode.java
+++ src/StringCode.java
@@ -2,6 +2,8 @@
 2023-08-19
 Day 5-1
 문자열 -> 배열
+break vs continue
+null vs 빈값
  */
 
 public class StringCode {
@@ -21,7 +23,7 @@ public class StringCode {
                     } else{ // codeArr[idx]==1이면
                        mode = 1;
                     }
-                    break;
+                    continue;
                 case 1:
                     if(codeArr[idx] != '1'){
                         if(idx%2 != 0){ // 홀수일 때
@@ -30,17 +32,37 @@ public class StringCode {
                     } else{ // codeArr[idx]==1이면
                         mode = 0;
                     }
-                    break;
-                default:
-                    ret = "EMPTY";
-                    break;
-            }
+                    continue;
+                }
         }
-
+        if(ret.length()==0){
+            return ret = "EMPTY";
+        }
         return ret;
     }
 
     public static void main(String[] args) {
-        System.out.println(solution("abc1abc1abc"));
+        System.out.println(solution("111"));
     }
 }
+
+/*
+class Solution {
+    public String solution(String code) {
+        StringBuilder answer = new StringBuilder();
+        int mode = 0;
+        for (int i = 0; i < code.length(); i++) {
+            char current = code.charAt(i);
+            if (current == '1') {
+                mode = mode == 0 ? 1 : 0;
+                continue;
+            }
+
+            if (i % 2 == mode) {
+                answer.append(current);
+            }
+        }
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
+    }
+}
+ */
