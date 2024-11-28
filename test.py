0c1491c5f7327489c098671d45908788bc75511f
sowon <leesowon00@jbnu.ac.kr>
2024-11-28 22:33:59 +0900

    241128

--- test.py
+++ test.py
@@ -1,15 +1,16 @@
+def solution(sizes):
+    answer = 0
+    max_row = max(sizes)[0]
+    max_row_idx = sizes.index(max_row)
+    max_col_idx = ''
 
-def solution(prices):
-    stack = []
-    answer = [0] * len(prices)
-    for i in range(len(prices)):
-        if stack != [] :
-            while stack !=[] and stack[-1][1] > prices[i] :
-                past, _ = stack.pop()
-                answer[past] = i-past
-        stack.append([i, prices[i]])
+    max_col = -float('inf')
+    for i in range(len(sizes[0])):
+        if max_col < sizes[i][1]:
+            max_col = sizes[i][1]
+            max_col_idx = int(i)
 
+    print(max_row_idx)
+    print(max_col_idx)
     return answer
 
-p = [1,1,1,1,1]
-print(solution(p))
